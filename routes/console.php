adc6b1e9ff71d917063205f39af530d71519fda5
Abdul Musavir Chukkan <musavirc17@gmail.com>
2025-04-19 04:02:15 +0530

    completed the overdue cron

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// This command will mark overdue tasks as expired
+Schedule::command('tasks:expire-overdue')->everyMinute();
